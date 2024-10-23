508fd4660f6d65ae90348ad949124a8c188917bc
awannoman <160035058+awannoman@users.noreply.github.com>
2024-10-23 08:09:30 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+// variables
+const accountId = 33344;
+let accountEmail = "uujrh2UHJHF2HJDFH2@HJSF";
+var accountPassward = "3342";
+accountCity = "karachi";
+let accountState;
+// accountId = 4 not allowed
+accountEmail = "njdfhh"
+accountPassward = "huy777"
+accountEmail = "kjfkkfow"
+
+console.log(accountId);
+console.table([accountEmail,accountPassward,accountCity,accountState])
+
+
+
+//    prefer do not use var
+//  because of issue in block scope and functional scope
